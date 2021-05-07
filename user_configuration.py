a07fbe0804f89e3e4046955cee9ef11a147aabb4
Katy Frank <katy.frank@tapjoy.com>
2021-05-07 17:25:27 -0400

    Adding user_configuration.py

---
+++ user_configuration.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Starting point for the user to convert data to screen if specimens are in idigbio or not
+
+"""
+#%% File Paths ################################################################
+#path to folder where all your inputs are stored
+# this needs to stay with double backslashes \\ otherwise the code will break!
+# so if you change it - use \\ not \ !
+OUTPUT_PATH = 'Z:\\deepblue_transfer\\Fish_Division\\iDigBio_Entries_Missing\\Week-11-16' 
+
+#The name of the folder containing files to screen
+#Should be the top level folder for a given week
+#Again the double \\ are important to this functioning correctly
+FOLDER_TO_SCREEN = 'Z:\\deepblue_transfer\\Fish_Division\\Week-11-16' 
+
+
+#This setting allows you to do a test run before actually moving the files
+TEST_RUN = True
